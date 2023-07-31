e9c69bcee96562d21e0fecdfcf1ce8892121b140
zousiyu1995 <zousiyu1995@outlook.com>
2023-07-31 11:32:22 +0800

    p724

---
+++ src/p724_find_pivot_index.rs
@@ -0,0 +1,29 @@
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        let nums1: Vec<i32> = vec![1, 7, 3, 6, 5, 6];
+        assert_eq!(pivot_index(nums1), 3);
+
+        let nums2: Vec<i32> = vec![1, 2, 3];
+        assert_eq!(pivot_index(nums2), -1);
+
+        let nums3: Vec<i32> = vec![2, 1, -1];
+        assert_eq!(pivot_index(nums3), 0);
+    }
+}
+
+pub fn pivot_index(nums: Vec<i32>) -> i32 {
+    let sum: i32 = nums.iter().sum();
+    let mut left: i32 = 0;
+    for i in 0..nums.len() {
+        if left == sum - nums[i] - left {
+            return i as i32;
+        }
+        left += nums[i];
+    }
+
+    -1
+}
