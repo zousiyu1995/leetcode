898b211e4ea6791b24c638cfea5ef31831e52a33
邹思宇 Siyu Zou <zousiyu1995@outlook.com>
2023-08-08 10:47:38 +0800

    p15，对撞双指针

--- src/p15_3sum.rs
+++ src/p15_3sum.rs
@@ -1,21 +1,22 @@
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[test]
+fn test() {
+    let v1: Vec<i32> = vec![-1, 0, 1, 2, -1, -4];
+    assert_eq!(three_sum(v1), vec![vec![-1, -1, 2], vec![-1, 0, 1]]);
 
-    #[test]
-    fn test() {
-        let v1: Vec<i32> = vec![-1, 0, 1, 2, -1, -4];
-        assert_eq!(three_sum(v1), vec![vec![-1, -1, 2], vec![-1, 0, 1]]);
+    let v2: Vec<i32> = vec![0, 1, 1];
+    let ans2: Vec<Vec<i32>> = vec![];
+    assert_eq!(three_sum(v2), ans2);
 
-        let v2: Vec<i32> = vec![0, 1, 1];
-        let ans2: Vec<Vec<i32>> = vec![];
-        assert_eq!(three_sum(v2), ans2);
-
-        let v3: Vec<i32> = vec![0, 0, 0];
-        assert_eq!(three_sum(v3), vec![vec![0, 0, 0]])
-    }
+    let v3: Vec<i32> = vec![0, 0, 0];
+    assert_eq!(three_sum(v3), vec![vec![0, 0, 0]])
 }
 
+// 对撞双指针
+// 枚举 i，问题变为两数之和
+// [x, x, x, x, x] 数组
+//  ^  ^        ^
+//  i  l        r  指针位置
+// i最多只能到数组的倒数第三个位置
 pub fn three_sum(mut nums: Vec<i32>) -> Vec<Vec<i32>> {
     // 不够三个数
     if nums.len() < 3 {
@@ -24,13 +25,10 @@ pub fn three_sum(mut nums: Vec<i32>) -> Vec<Vec<i32>> {
 
     // sort
     nums.sort();
+
     let mut ans: Vec<Vec<i32>> = vec![];
-    // [x, x, x, x, x] 数组
-    //  ^  ^        ^
-    //  i  l        r  指针位置
-    // i最多只能到数组的倒数第三个位置
     for i in 0..(nums.len() - 2) {
-        // i的去重
+        // i 的去重。只要当前枚举的 i 和上一个 i 相同，跳过这个 i，因为上一次已经处理过这个 i 了
         if i > 0 && nums[i] == nums[i - 1] {
             continue;
         }
@@ -42,11 +40,9 @@ pub fn three_sum(mut nums: Vec<i32>) -> Vec<Vec<i32>> {
             if sum > 0 {
                 // 太大了，缩右边界
                 r -= 1;
-                continue;
             } else if sum < 0 {
                 // 太小了，缩左边界
                 l += 1;
-                continue;
             } else {
                 // =0，找到了，先去重
                 while l < r && nums[l] == nums[l + 1] {
@@ -59,7 +55,6 @@ pub fn three_sum(mut nums: Vec<i32>) -> Vec<Vec<i32>> {
                 // 同时缩左右边界，找下一个
                 l += 1;
                 r -= 1;
-                continue;
             }
         }
     }
