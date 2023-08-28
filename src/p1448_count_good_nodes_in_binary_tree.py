2783baf4abee610cbf7b8a31fd08c737da48bcec
邹思宇 Siyu Zou <zousiyu1995@outlook.com>
2023-08-28 09:30:32 +0800

    p1448，二叉树

--- src/p1448_count_good_nodes_in_binary_tree.py
+++ src/p1448_count_good_nodes_in_binary_tree.py
@@ -9,7 +9,9 @@ class Solution:
         if root is None:
             return 0
 
+        # 递归左子树，获取左子树的好节点个数
         left = self.goodNodes(root.left, max(mx, root.val))
+        # 递归右子树，获取右子树的好节点个数
         right = self.goodNodes(root.right, max(mx, root.val))
 
         return left + right + (mx <= root.val)
