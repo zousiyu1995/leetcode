18b9ff0f37241d344c8fa8fd24ceffb97a78774c
邹思宇 Siyu Zou <zousiyu1995@outlook.com>
2023-08-29 16:22:51 +0800

    p206，链表，test

--- src/p206_reverse_linked_list.rs
+++ src/p206_reverse_linked_list.rs
@@ -1,5 +1,14 @@
 #[test]
-fn test() {}
+fn test() {
+    let mut l: ListNode = ListNode::new(1);
+    for i in 2..=5 {
+        l.push(i);
+    }
+    l.print();
+
+    let r: Option<Box<ListNode>> = reverse_list(Some(Box::new(l)));
+    r.unwrap().print();
+}
 
 use super::util::list_node::ListNode;
 
@@ -7,7 +16,7 @@ pub fn reverse_list(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
     let mut pre: Option<Box<ListNode>> = None;
     let mut cur: Option<Box<ListNode>> = head;
 
-    // None 1 -> 2 -> 3 -> 4
+    // None 1 -> 2 -> 3 -> 4 -> 5
     //  ^   ^
     // pre  cur
     // 只要当前节点不为空
