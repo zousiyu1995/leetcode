bfa7fc209d435d2c090fb0eb0530763b81e83d17
邹思宇 Siyu Zou <zousiyu1995@outlook.com>
2023-08-29 16:22:26 +0800

    impl ListNode

--- src/util/list_node.rs
+++ src/util/list_node.rs
@@ -13,7 +13,35 @@ impl ListNode {
 }
 
 // 自定义方法
+impl ListNode {
+    // l: 1->2
+    // l.push(3): 1->2->3
+    pub fn push(&mut self, val: i32) {
+        // 新建node
+        let node: Option<Box<ListNode>> = Some(Box::new(ListNode::new(val)));
 
+        // self就是head节点
+        // 遍历到tail节点，如果tail.next=None，设置tail.next=node
+        let mut tail: &mut ListNode = self;
+        while let Some(ref mut next) = tail.next {
+            tail = next;
+        }
+        tail.next = node;
+    }
+
+    pub fn print(&self) {
+        let mut vals: Vec<String> = vec![];
+        let mut tail: &ListNode = self;
+        // 遍历节点，只要不为空
+        while let Some(ref next) = tail.next {
+            vals.push(format!("{}", &tail.val));
+            tail = next;
+        }
+        vals.push(format!("{}", tail.val));
+
+        println!("{}", vals.join(" -> "));
+    }
+}
 // 从数组创建链表
 // impl<const N: usize> From<[i32; N]> for ListNode {
 //     // from [1, 2, 3, 4, 5]
