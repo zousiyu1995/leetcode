3c53d34d3354a1a1e3b3224b9ba5a9a915d69755
zousiyu1995 <zousiyu1995@outlook.com>
2023-11-06 19:51:07 +0800

    p1，哈希表

--- src/p1_two_sum.rs
+++ src/p1_two_sum.rs
@@ -1,20 +1,14 @@
-use std::collections::HashMap;
+#[test]
+fn test() {
+    let v1: Vec<i32> = vec![2, 7, 11, 15];
+    assert_eq!(two_sum(v1, 9), vec![0, 1]);
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test() {
-        let v1: Vec<i32> = vec![2, 7, 11, 15];
-        assert_eq!(two_sum(v1, 9), vec![0, 1]);
-
-        let v2: Vec<i32> = vec![3, 2, 4];
-        assert_eq!(two_sum(v2, 6), vec![1, 2]);
-    }
+    let v2: Vec<i32> = vec![3, 2, 4];
+    assert_eq!(two_sum(v2, 6), vec![1, 2]);
 }
 
 pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    use std::collections::HashMap;
     let mut map: HashMap<i32, i32> = HashMap::new();
 
     for (idx, num) in nums.into_iter().enumerate() {
@@ -24,7 +18,9 @@ pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
                 return vec![*x, idx as i32];
             }
             // 如果不在map里，加进去
-            None => map.insert(num, idx as i32),
+            None => {
+                map.insert(num, idx as i32);
+            }
         };
     }
 
